68f64033fc85d178cbfe992b352f7b4e9b579d79
Patorntarid2546 <chpooha@gmail.com>
2021-11-26 19:10:00 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int M = 0;
+    int N = 0;
+    int O = 0; 
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> M;
+    while(M != 0){
+        if(M%2 == 0){
+        cout << "Enter an integer: ";
+        cin >> M;
+            N = N + 1;
+        }else{
+        cout << "Enter an integer: ";
+            cin >> M;
+            O = O + 1;
+        }
+        
+    }
+        
+    cout << "#Even numbers = " << N << endl;
+    cout << "#Odd numbers = " << O << endl ;
     return 0;
 }
